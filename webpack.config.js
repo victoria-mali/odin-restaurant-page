d9394f29da6a2b657828f3f816f5526c2426fbc4
victoria_mali <153543182+victoria-mali@users.noreply.github.com>
2026-06-23 17:19:23 +0100

    Design the homepage

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,11 @@ export default {
 {
   test: /\.(png|svg|jpg|jpeg|gif)$/i,
   type: "asset/resource",
-}
+},
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
 };
